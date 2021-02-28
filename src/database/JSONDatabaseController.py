53d90e39788b5acc805a5f14f00287b8768f7fc4
FarhanChowdhury248 <farhanChowdhury248@users.noreply.github.com>
2021-02-28 13:38:23 -0500

    completed collection methods

--- src/database/JSONDatabaseController.py
+++ src/database/JSONDatabaseController.py
@@ -1,32 +1,68 @@
 import os
 from DatabaseControllerInterface import DatabaseControllerInterface
+import json
 
 DATA_FILE_TYPE = '.json'
 PATH_TO_DATABSE = './data'
 
-def get_jsons(path):
-    return [file[:-len(DATA_FILE_TYPE)] for file in os.listdir(path) if file.endswith(DATA_FILE_TYPE)]
+def get_jsons():
+    return [file[:-len(DATA_FILE_TYPE)] for file in os.listdir(PATH_TO_DATABSE) if file.endswith(DATA_FILE_TYPE)]
 
-class JSONDatabaseController():
+def get_json_file_path(file_name):
+    return '{}/{}{}'.format(PATH_TO_DATABSE, file_name, DATA_FILE_TYPE)
+
+class JSONDatabaseController(DatabaseControllerInterface):
     def create_collection(self, col_name):
-        with open('{}/{}{}'.format(PATH_TO_DATABSE, col_name, DATA_FILE_TYPE), 'a+') as f:
-            f.seek(0)
-            if f.read() == '':
-                f.write('{}')
-                print('Created new collection')
-            else:
-                print('ERROR: collection already exists')
+        if col_name in get_jsons():
+            print('ERROR: collection already exists')
+            return
+        with open(get_json_file_path(col_name), 'w') as f:
+            json.dump({}, f)
+            print('Created collection')
 
     def delete_collection(self, col_name):
-        if col_name in get_jsons(PATH_TO_DATABSE):
-            os.remove(PATH_TO_DATABSE + '/' + col_name + DATA_FILE_TYPE)
+        if col_name in get_jsons():
+            os.remove(get_json_file_path(col_name))
             print('Removed collection')
         else:
             print('ERROR: collection does not exist')
 
-db = JSONDatabaseController()
+    def get_collection(self, col_name):
+        if col_name in get_jsons():
+            with open(get_json_file_path(col_name)) as f:
+                data = json.load(f)
+            return data
+        print('ERROR: collection does not exist')
 
-db.create_collection('hello')
-db.create_collection('hello')
-db.delete_collection('hello')
-db.delete_collection('hello')
+    def clear_collection(self, col_name):
+        if col_name in get_jsons():
+            with open(get_json_file_path(), 'w') as f:
+                json.dump({}, f)
+                print('Cleared collection')
+        else:
+            print('ERROR: collection does not exist')
+
+    def add_document(self, doc_name, doc_data = {}):
+        pass
+    def update_document(self, doc_name, doc_data = {}):
+        pass
+    def delete_document(self, doc_name):
+        pass
+    def get_document(self, doc_name):
+        pass
+    def clear_document(self, doc_name):
+        pass
+
+if __name__ == '__main__':
+    db = JSONDatabaseController()
+
+    # test set 1
+    db.create_collection('hello')
+    db.create_collection('hello')
+    print(db.get_collection('hello'))
+    db.delete_collection('hello')
+    db.delete_collection('hello')
+    print(db.get_collection('hello'))
+
+    # test set 2
+    
